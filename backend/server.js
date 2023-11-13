599a3324da1446d4c79c76f471b908ee10bdf226
ZainIslam345 <zainislam345@gmail.com>
2023-11-13 19:28:31 +0500

    cors error done

--- backend/server.js
+++ backend/server.js
@@ -8,52 +8,24 @@ const connectDB = require('./config/db')
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 5000
 
-// const dev = process.env.NODE_ENV !== 'production';
-// const next = require('next');
-// const nextjs = next({ dev });
-// const handle = nextjs.getRequestHandler();
-
-
-
 
 connectDB()
 const app = express();
 
-// app.use(cors());
 const corsOptions = {
-    origin: 'http://localhost:3000',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+    origin: '*', // Allow any origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
 };
 
 app.use(cors(corsOptions));
-
-  
-  
-  
-  
   
 app.use(express.json())
 app.use(express.urlencoded({extended: false}));
 
 app.use('/api/todos',require('./routes/todosRoutes'))
 app.use('/api/users',require('./routes/userRoutes'))
-
-
-// if (process.env.NODE_ENV === 'production') {
-    // app.use(express.static(path.join(__dirname, '../frontend/build')));
-    // app.use(express.static(path.join(__dirname, '../frontend/public')));
-    // app.all('*', (req, res) => {
-    //     return handle(req, res);
-    // });
-  
-    // app.get('*', (req, res) =>
-    //   res.sendFile(
-    //     path.resolve(__dirname, '../', 'frontend', '.next', 'index.html')
-    //   )
-    // );
-//   } else {
-//     app.get('/', (req, res) => res.send('Please set to production'));
-// }
 
 app.use(errorHandler)
 app.listen(PORT, () => console.log(`Server Running on port ${PORT}`))
