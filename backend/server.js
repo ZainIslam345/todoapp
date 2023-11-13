efb859fa22e5b2a5969e7b41adfe3015520667c4
ZainIslam345 <zainislam345@gmail.com>
2023-11-13 18:45:53 +0500

    fix cors error

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,19 @@ const PORT = process.env.PORT || 5000
 connectDB()
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
+
+  
+  
+  
+  
+  
 app.use(express.json())
 app.use(express.urlencoded({extended: false}));
 
